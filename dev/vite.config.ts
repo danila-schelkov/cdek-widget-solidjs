f5ac4e6141cafc6595e8885f449ba020282c3021
danila-schelkov <danila-schelkov@users.noreply.github.com>
2025-07-18 16:06:29 +0000

    Format

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import solidPlugin from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import path from "node:path";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
     resolve: {
         alias: {
-            src: path.resolve(__dirname, '../src'),
+            src: path.resolve(__dirname, "../src"),
         },
     },
     plugins: [
         solidPlugin(),
         {
-            name: 'Replace env variables',
+            name: "Replace env variables",
             transform(code, id) {
-                if (id.includes('node_modules')) {
-                    return code
+                if (id.includes("node_modules")) {
+                    return code;
                 }
                 return code
-                    .replace(/process\.env\.SSR/g, 'false')
-                    .replace(/process\.env\.DEV/g, 'true')
-                    .replace(/process\.env\.PROD/g, 'false')
+                    .replace(/process\.env\.SSR/g, "false")
+                    .replace(/process\.env\.DEV/g, "true")
+                    .replace(/process\.env\.PROD/g, "false")
                     .replace(/process\.env\.NODE_ENV/g, '"development"')
-                    .replace(/import\.meta\.env\.SSR/g, 'false')
-                    .replace(/import\.meta\.env\.DEV/g, 'true')
-                    .replace(/import\.meta\.env\.PROD/g, 'false')
-                    .replace(/import\.meta\.env\.NODE_ENV/g, '"development"')
+                    .replace(/import\.meta\.env\.SSR/g, "false")
+                    .replace(/import\.meta\.env\.DEV/g, "true")
+                    .replace(/import\.meta\.env\.PROD/g, "false")
+                    .replace(/import\.meta\.env\.NODE_ENV/g, '"development"');
             },
         },
     ],
@@ -32,6 +32,6 @@ export default defineConfig({
         port: 3000,
     },
     build: {
-        target: 'esnext',
+        target: "esnext",
     },
-})
+});
